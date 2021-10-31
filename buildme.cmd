a654a0d030d0e6636393ea7129547251f75efe82
pencaster <Krum@Haesli.net>
2021-10-31 07:05:43 +0100

    sync

--- buildme.cmd
+++ buildme.cmd
@@ -3,3 +3,12 @@ cd .\js
 yarn install
 yarn bootstrap
 yarn build
+
+cd packages/web
+yarn deploy
+
+
+$ git checkout -b metaplex-foundation-master-30-10-21  master
+$ git pull https://github.com/metaplex-foundation/metaplex.git master
+$ git checkout master
+$ git merge --no-ff metaplex-foundation-master-30-10-21
